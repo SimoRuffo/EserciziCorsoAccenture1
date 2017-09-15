0b2262c8613d88435adfa9fc6e31555efc47d91b
SimoRuffo <31985927+SimoRuffo@users.noreply.github.com>
2017-09-15 11:55:20 +0200

    Update Main.java

--- Stringhe/src/Main.java
+++ Stringhe/src/Main.java
@@ -1,6 +1,8 @@
 
 public class Main {
 
+	public class Main {
+
 	public static void main(String[] args) {
 		String nome = "Giovanni";
 		String nomeDue = "Giovanni";
@@ -19,6 +21,13 @@ public class Main {
 		} else {
 			System.out.println("I nomi sono diverso");
 		}
+		
+		
+		/***************************************************************************/
+		
+		// Creiamo due automobili utilizzando new Automobile(.....)...
+		Automobile fiatUno = new Automobile(1450, "Uno", "bianca", "Fiat", "MN245VB", new Date);
+		Automobile fiatPunto = new Automobile(1450, "Punto", "rossa", "Fiat", "VN535CB", new Date);	
 
 	}
 
